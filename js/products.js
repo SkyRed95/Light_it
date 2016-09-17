238557f7236653d78e5c7a430ec4de5ebb41a9fa
SkyRed <19skyred95@gmail.com>
2016-09-17 22:31:54 +0300

    angular_title

--- js/products.js
+++ js/products.js
@@ -1,16 +1,10 @@
 $(document).ready(function () {
     var productsAPI = "http://smktesting.herokuapp.com/api/products/?format=json";
-    var static = "http://smktesting.herokuapp.com/static/";
-    //var arr_for_server = new Array();
-    
-    $.getJSON(productsAPI, function (data) {
-        $.each(data, function (i, record) {
-            document.write('<ul><li>' + record.title + '</li></ul>');
-            document.write('<li><h2>' + record.id + '</h2></li>');
-            document.write('<img src="' + static + record.img + '">');
-            document.write('<p>' + record.title + '</p>');
-            document.write('<li>' + record.text + '</li>');
-            //arr_for_server[i] = [[record.id], [record.title], [record.img], [record.text]]
-        });
+    var app = angular.module("MyApp", []);
+    app.controller("TitlesCtrl", function($scope, $http) {
+        $http.get(productsAPI).
+        success(function(data, title) {
+            $scope.titles = data;
+        })
     });
 });
